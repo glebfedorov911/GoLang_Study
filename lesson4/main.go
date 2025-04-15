4a8741bff217ce2d1d13cd9a76254f78a05ba281
Gleb <fedorov22134@gmail.com>
2025-04-16 01:09:17 +0300

    add lesson4 (exe)

---
+++ lesson4/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/*
+go build lesson4/main.go
+go build -ldflags "-s -w" lesson4/main.go
+*/
+
+func main() {
+	fmt.Println("Введите члены квадратного уравнения a, b, c")
+
+	var a, b, c float64
+	fmt.Scan(&a, &b, &c)
+
+	var D float64
+	D = (b*b - 4*a*c)
+	if D > 0 {
+		var x1, x2 float64
+		x1 = (-b + math.Sqrt(D)) / (2 * a)
+		x2 = (-b - math.Sqrt(D)) / (2 * a)
+		fmt.Println("Найдено 2 корня:", x1, x2)
+	} else if D == 0 {
+		var x float64
+		x = (-b) / (2 * a)
+		fmt.Println("Найден 1 корень:", x)
+	} else {
+		fmt.Println("Корни не найдены, D < 0")
+	}
+
+	fmt.Scan(&a)
+}
